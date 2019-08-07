d80871975df2cc06dcd1109380bce6c92adb3605
Rafael Amauri <49131143+RADA77777@users.noreply.github.com>
2019-08-07 08:44:30 -0300

    Add files via upload

--- Periodo2/AED2/TP1/Palindromo.java
+++ Periodo2/AED2/TP1/Palindromo.java
@@ -1,24 +1,64 @@
 public class Palindromo
 {
-	public static void main(String[] arguments)
+	public static void main(String[] args)
 	{
-		String entrada = MyIO.readLine();
-		entrada = removeEspacos(entrada);
-		System.out.println(entrada);
+		char[] entrada = new char[100];
+		String str = MyIO.readLine();
+		int tam = str.length();
+		
+
+		char[] entradaReversa = new char[100];
+		entrada = str2char(str);
+		entrada = removeEspaco(entrada);
+		entradaReversa = reverterPalavra(entrada);
+
+	}
+
+
+	/*
+	 * Converte de String para array de caracteres
+	 */
+	public static char[] str2char(String str)
+	{
+		char[] entrada = new char[100];
+		for(int i = 0; i < str.length(); i++)
+		{
+			entrada[i] = str.charAt(i);
+		}
+		return entrada;
 	}
 	
-	public static String removeEspacos(String string)
+
+	/*
+	 * Recebe um char[] e remove todos espacos dele 
+	 */
+	public static char[] removeEspaco(char[] entrada)
 	{
-		for(int i = 0; i < string.length(); i++)
+		for(int i = 0; i < entrada.length; i++)
 		{
-			if(string.charAt(i) == ' ')
+			if(entrada[i] == ' ')
 			{
-				string = string.toCharArray();
-			       	string[i] = string.charAt(i+i);
-				string = string.toString();
-				string = removeEspacos(string);
+				for(int j = 0; j < (entrada.length - 1); j++)
+				{
+					entrada[j] = entrada[j+1];
+				}
+				entrada = removeEspaco(entrada);
 			}
 		}
-		return string;
+		return entrada;
+	}
+
+	public static char[] reverterPalavra(char[] entrada)
+	{
+		char aux;
+		int cont = 0;
+		for(int i = entrada.length - 1; i > (entrada.length/2); i--)
+		{
+			aux = entrada[cont];
+			entrada[cont] = entrada[i];
+			entrada[i] = aux;
+			cont++;
+		}
+		return entrada;
 	}
 }
