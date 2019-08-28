6fb63b0f8050603b034e377e540cd6b20714bb78
Rafael Amauri <49131143+RADA77777@users.noreply.github.com>
2019-08-28 10:12:48 -0300

    Add files via upload

---
+++ times.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+ 
+public class times {
+    public static void main(String[] args) throws Exception {
+	    
+	    Scanner in = new Scanner(System.in);
+	    String linha;
+	    boolean notFim;
+	    String nomeTime = new String();
+            
+	    do{
+                        nomeTime = in.nextLine();
+                        notFim = notFim(nomeTime); // Se nome do time for diferente de FIM, a execucao continua
+                        if(notFim)
+                        {
+	    			BufferedReader reader = new BufferedReader(new FileReader(nomeTime));
+               			
+				while ((linha = reader.readLine()) != null)
+                		{
+                        		if(linha.contains("infobox vcard"))
+                        		{
+                                		linha = removerEntreTags(linha, "<", ">");
+                                		linha = removerEntreTags(linha, "&", ";");
+                                		System.out.println(linha);
+                        		}
+
+                		}
+                		reader.close();
+            		}
+
+		}while(notFim);
+    }
+    public static String removerEntreTags(String linha, String tag1, String tag2)
+    {
+	linha = linha.replaceAll("(?s)" + tag1 + ".*?" + tag2, "");
+	return linha;
+    }
+
+
+    public static boolean notFim(String string)
+    {
+	    boolean notFim = false;
+	    
+	    if(string.charAt(0) != 'F' || string.charAt(1) != 'I' || string.charAt(2) != 'M')
+	    {
+		    notFim = true;
+	    }
+	    return notFim;
+    }
+}
